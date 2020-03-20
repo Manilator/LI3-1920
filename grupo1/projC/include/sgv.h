e0a7c8447175793c79ee4b7ebfea0c466618ddca
Manilator <manilator777@gmail.com>
2020-03-20 15:25:24 +0000

    update include files

--- grupo1/projC/include/sgv.h
+++ grupo1/projC/include/sgv.h
@@ -3,6 +3,30 @@
 
 typedef struct sgv* SGV;
 
-int startSGV();
+typedef struct startValues* StartValues;
+
+SGV startSGV(StartValues sv);
+
+SGV initSGV();
+
+StartValues initStartValues();
+
+char* getClientsPath(StartValues sv);
+
+char* getProductsPath(StartValues sv);
+
+char* getSalesPath(StartValues sv);
+
+int getValidClients(StartValues sv);
+
+int getValidProducts(StartValues sv);
+
+int getValidSales(StartValues sv);
+
+int getReadClients (StartValues sv);
+
+int getReadProducts (StartValues sv);
+
+int getReadSales (StartValues sv);
 
 #endif
--- grupo1/projC/include/view.h
+++ grupo1/projC/include/view.h
@@ -1,5 +1,14 @@
 #ifndef ___VIEW_H___
 #define ___VIEW_H___
 
+void viewPrintStartValues(char *clients_path,
+                          char *products_path,
+                          char *sales_path,
+                          int   valid_clients,
+                          int   valid_products,
+                          int   valid_sales,
+                          int   clients_read,
+                          int   products_read,
+                          int   sales_read);
 
 #endif
