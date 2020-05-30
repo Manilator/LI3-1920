2ec9929baa3a67d3c2852bdb6c4b903a085396ac
Paulo <paulojorgelima18@gmail.com>
2020-05-30 17:52:29 +0100

    more useless code removal

--- grupo1/projJava/src/model/Client.java
+++ grupo1/projJava/src/model/Client.java
@@ -9,23 +9,9 @@ public class Client implements IClient {
     private final char letter; /**< Primeira letra no código de cliente */
     private final int number; /**< Número presente no código de cliente */
 
-    public Client(char letter, int number) {
-        this.letter = letter;
-        this.number = number;
-    }
-
     public Client (String clientCode) {
         this.letter = clientCode.charAt(0);
         this.number = Integer.parseInt(clientCode.substring(1));
-    }
-
-    public static String getClientCode(Client client) {
-        return String.format("%c%d",client.getLetter(),client.getNumber());
-    }
-
-
-    Client newClient(String clientCode) {
-        return new Client(clientCode.charAt(0), Integer.parseInt(clientCode.substring(1)));
     }
 
     public boolean validate() {
@@ -35,20 +21,4 @@ public class Client implements IClient {
         return e.matches();
     }
 
-    public Boolean equalsClient(Client a, Client b) {
-        return (a.getLetter() == b.getLetter() && a.getNumber() == b.getNumber());
-    }
-
-    public char getLetter() {
-        return letter;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    /*
-    void destroyClient(Client client) {
-        free(client);
-    }*/
 }
